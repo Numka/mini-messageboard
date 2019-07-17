034d13c64fa2b30efc1c4aa16a4f9ed47da4ac6b
Numinor <numinor93@mail.ru>
2019-07-17 13:40:31 +0300

    Added ability to add messages

--- app.js
+++ app.js
@@ -6,4 +6,16 @@ const app = express();
 
 app.set('view engine', 'pug');
 
+const newRouter = require('./routes/new');
+const mainRouter = require('./routes/main');
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(mainRouter);
+app.use(newRouter);
+
+app.use((req, res, next) => {
+  res.status(404).render('404');
+})
+
 app.listen(3000);
--- routes/main.js
+++ routes/main.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
+const messages = [{ text: "Hi There!", user: "Amando", added: new Date()},
+{ text: "Hello World!", user: "Greg", added: new Date()}];
+
+router.post('/new', (req, res, next) => {
+  messages.push({text: req.body.message, user: req.body.user, added: new Date()});
+  res.redirect('/');
+});
+
 router.get('/', (req, res, next) => {
-  res.render('main', { title: 'Main' });
+  res.render('main', { title: 'Main', mes: messages });
 });
 
 module.exports = router;
